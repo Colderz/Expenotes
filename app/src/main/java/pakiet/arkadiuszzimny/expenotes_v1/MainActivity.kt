a049fa2775616239091b9d790d5a54c8a6d4eebb
Colderz <arekzim98@gmail.com>
2021-01-25 22:18:56 +0100

    Inital commit

---
+++ app/src/main/java/pakiet/arkadiuszzimny/expenotes_v1/MainActivity.kt
@@ -0,0 +1,11 @@
+package pakiet.arkadiuszzimny.expenotes_v1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
