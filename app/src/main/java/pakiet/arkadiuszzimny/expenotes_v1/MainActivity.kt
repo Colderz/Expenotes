f0498a1ab1fc6c4186db0517d43be85f448db979
Colderz <arekzim98@gmail.com>
2021-01-26 22:19:29 +0100

    Change of navigation view attributes, customization

--- app/src/main/java/pakiet/arkadiuszzimny/expenotes_v1/MainActivity.kt
+++ app/src/main/java/pakiet/arkadiuszzimny/expenotes_v1/MainActivity.kt
@@ -2,14 +2,11 @@ package pakiet.arkadiuszzimny.expenotes_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+
 import kotlinx.android.synthetic.main.activity_main.*
-import pakiet.arkadiuszzimny.expenotes_v1.ui.fragments.ConverterFragment
-import pakiet.arkadiuszzimny.expenotes_v1.ui.fragments.DoneFragment
-import pakiet.arkadiuszzimny.expenotes_v1.ui.fragments.PlansFragment as PlansFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bottomNavigationView.setupWithNavController(NavHostFragment.findNavController())
+
     }
 }
