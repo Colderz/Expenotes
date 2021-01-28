ae697d7f8c10f26482b825febcdc3cc310868e8b
Colderz <arekzim98@gmail.com>
2021-01-28 22:05:54 +0100

    Significant converter view design, base element position modification

--- app/src/main/java/pakiet/arkadiuszzimny/expenotes_v1/ui/fragments/ConverterFragment.kt
+++ app/src/main/java/pakiet/arkadiuszzimny/expenotes_v1/ui/fragments/ConverterFragment.kt
@@ -1,7 +1,23 @@
 package pakiet.arkadiuszzimny.expenotes_v1.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.NumberPicker
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_converter.*
 import pakiet.arkadiuszzimny.expenotes_v1.R
 
 class ConverterFragment : Fragment(R.layout.fragment_converter) {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
 }
